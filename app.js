4522c4f83610c4d7b531deacc52c716c3d142b5f
Aleksandr Mihhailov <sasha.mihhailov@gmail.com>
2016-05-28 22:29:19 +0300

    added locales and moved templates

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var i18n = require('i18n');
 
 var app = express();
+
+i18n.configure({
+  locales: ['en', 'ru'],
+  directory: [__dirname, 'locales'].join('/'),
+  defaultLocale: 'en',
+  queryParameter: 'lang'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(i18n.init);
 
-app.use('/', routes);
+app.get('/', function(req, res, next) {
+  res.render('index', {});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
